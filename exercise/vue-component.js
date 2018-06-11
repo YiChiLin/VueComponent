52e3cc95b8ff7e433033abbba9f262e6dd65b522
jean.lin52 <jean.lin52@TI03BY32>
2018-06-11 18:27:56 +0800

    Implement

--- exercise/vue-component.js
+++ exercise/vue-component.js
@@ -1,3 +1,24 @@
-Vue.component('component-name', {
-  template:`<div></div>`
+Vue.component('jeancomponent', {
+  template:`<div @click="add()" style="border:1px solid black; height:20px; width:310px;">
+              <div v-for="item in this.count" style="display:inline-block; height:20px; width:30px; background:green; border-right:1px solid black;"></div>
+            </div>`,
+   methods: {
+    add: function () {
+        if(this.count.length == 10){
+          this.count = [];
+          return;
+        }
+        this.count.push('hi');      
+    }
+  },
+    data:function (){
+      return{
+           count: []
+      }
+  }
+})
+
+new Vue({
+  el: "#app",
+
 })
